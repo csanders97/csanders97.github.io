e3d1bf97a5fbe819a9a2acf93df9df6f1f27bfe8
Caleb Sanders <colonelsanders217@gmail.com>
2018-01-16 15:54:34 -0700

    Got all routes working. Now I need to populate them

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,11 @@ module.exports = (app) => {
     
     app.get('/', route.home);
 
+    app.get('/about', route.about);
+
     app.get('/work', route.work);
+
+    app.get('/resume', route.resume);
+
+    app.get('/contact', route.contact);
 }
--- routes/route.js
+++ routes/route.js
@@ -4,6 +4,19 @@ exports.home = function(req, res) {
     res.render('home');
 }
 
+exports.about = function(req, res) {
+    res.render('about');
+}
+
 exports.work = function(req, res) {
     res.render('work');
+}
+
+exports.resume = function(req, res) {
+    res.render('resume');
+}
+
+exports.contact = function(req, res) {
+    res.render('contact');
+    console.log("Tester");
 }
